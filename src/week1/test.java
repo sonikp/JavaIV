6dea3e84a29f391a1ca94873017b0839c7c6b15c
notroot <notroot@email.server>
2017-07-18 08:53:38 -0700

    upload

--- src/week1/test.java
+++ src/week1/test.java
@@ -1,10 +1,10 @@
 package week1;
 
-public class test
-{
-	public static void main(String[] args)
-	{
-		System.out.println("test");
-		System.out.println(System.getProperties());
+// test for policy manager
+// # java -Djava.security.manager -Djava.security.policy=test.policy EvilEmpire
+
+public class test {
+	public static void main(String[] args) {
+		System.out.println("132");
 	}
 }
