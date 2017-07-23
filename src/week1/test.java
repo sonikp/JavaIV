b3a0fd05a1712105a94120bb258ab76a84436a20
notroot <notroot@email.server>
2017-07-22 17:02:55 -0700

    upload

--- src/week1/test.java
+++ src/week1/test.java
@@ -5,6 +5,19 @@ package week1;
 
 public class test {
 	public static void main(String[] args) {
+		
+		int high = 6;
+		int low = 4;
+		
+		System.out.println("orig (4 + 6) \\ 2 = " + ((low + high) / 2) );
+		System.out.println("bttr 4 + (6 - 4) \\ 2) = " + (low + ((high - low) / 2)) );
+		System.out.println("last (4 + 6) >>> 1 = " + ((low + high) >>> 1));
+		
+
+		
+		
+		
+		
 		System.out.println("132");
 	}
 }
