98067ac838c9249223bf4364770f01629eea8194
notroot <notroot@email.server>
2017-08-14 18:30:27 -0700

    upload

---
+++ src/week5class/EX_ParseURL.java
@@ -0,0 +1,10 @@
+package week5class;
+
+import java.net.URL;
+
+public class EX_ParseURL {
+	
+	public static void main(String[] args) throws Exception {
+		URL aURL = new URL("http://www.google.com");
+	}
+}
