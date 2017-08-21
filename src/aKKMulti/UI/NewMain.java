0b209808ecb50e84fa506d948d9a8c184c851a3f
notroot <notroot@email.server>
2017-08-20 19:42:35 -0700

    upload

--- src/aKKMulti/UI/NewMain.java
+++ src/aKKMulti/UI/NewMain.java
@@ -6,7 +6,7 @@ public class NewMain {
 	
 	public static void main(String[] args) {
 		
-		NewMultiServer app = new NewMultiServer();
+		NewMultiServer app = new NewMultiServer(null);
 		app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 	}
