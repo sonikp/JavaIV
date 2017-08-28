e1c1735fa3a46665f17bf837b8a6be30a746fd10
notroot <notroot@email.server>
2017-08-28 15:19:46 -0700

    upload

---
+++ src/a_client_work/Launch.java
@@ -0,0 +1,16 @@
+package a_client_work;
+
+import javax.swing.JFrame;
+
+public class Launch extends JFrame {
+	
+	
+	public Launch () {
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
